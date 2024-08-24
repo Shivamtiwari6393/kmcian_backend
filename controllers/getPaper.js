9fd823f4cb3f2de7a7d1f2e8ea5da9db4ed09dbf
Shivam Tiwari <shivamtiwari6223@gmail.com>
2024-08-24 17:01:05 +0530

    updated getPaper for multi option request, added requestedPaper object

--- controllers/getPaper.js
+++ controllers/getPaper.js
@@ -3,12 +3,23 @@ const Paper = require("../models/paperModel")
 const getPaper = async (req, res) => {
 
 
-    const { course, branch, semester, year ,downloadable} = req.body
+    const { course, branch, semester, year, downloadable } = req.body
+
+
+        const requestedData = {
+            course: course,
+            downloadable: downloadable,
+        }
+
+        if(semester) requestedData.semester = semester
+        if(year) requestedData.year = year
+        if(branch) requestedData.branch = branch
+
 
     
     try {
-        const reqPaper = await Paper.find({ course: course, branch: branch, semester: semester, downloadable: downloadable , year: year}, { pdf: 0, pdfContentType: 0 })
-        
+        const reqPaper = await Paper.find({ ...requestedData }, { pdf: 0, pdfContentType: 0 })
+
         if (reqPaper.length === 0) {
             return res.status(404).json({ error: "No papers found" });
         }
