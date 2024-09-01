0fb641bd56a5e6598f7d0837135ff3dffeced24b
Shivam Tiwari <shivamtiwari6223@gmail.com>
2024-09-01 15:33:55 +0530

    added user auth

--- routes/paperRoute.js
+++ routes/paperRoute.js
@@ -8,15 +8,15 @@ const multer = require('multer');
 const updatePaper  = require("../controllers/UpdatePaper");
 const deletePaper = require("../controllers/deletePaper")
 const upload = multer({ storage: multer.memoryStorage() })
-
+const protect = require("../middleware/auth")
 
 router.post(`/api/paper`, getPaper)
 router.post('/api/paper/upload', upload.single('pdf'), postPaper)
 
 router.get('/api/paper/download',downloadPaper);
 
-router.put('/api/paper/update/:id',upload.single('updatedpdf'),updatePaper)
+router.put('/api/paper/update/:id',protect,upload.single('updatedpdf'),updatePaper)
 
-router.delete('/api/paper/delete/:course/:id',deletePaper)
+router.delete('/api/paper/delete/:course/:id',protect,deletePaper)
 
 module.exports = router
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,107 +2,120 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const User = require('../models/userModel');
+const jwt = require('jsonwebtoken')
 
 const router = express.Router();
 
 
-router.get('/user', async (req, res) => {
-    try {
-      const user = await User.findOne(); // Adjust as needed for user identification
-      res.json(user);
-    } catch (error) {
-      res.status(500).json({ error: 'Server error' });
-    }
+
+// ===============generate taken====================
+
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '1d',
   });
+};
 
 
-  // Update user data (no authentication middleware here)
+
+
+// ============== update user =========================
+
+
 
 router.put('/user', async (req, res) => {
-    const { username, email, password } = req.body;
-  
-    try {
-      const user = await User.findOne(); // Adjust as needed for user identification
-  
-      if (!user) {
-        return res.status(404).json({ error: 'User not found' });
-      }
-  
-      user.username = username || user.username;
-      user.email = email || user.email;
-  
-      if (password) {
-        user.password = await bcrypt.hash(password, 10);
-      }
-  
-      await user.save();
-      res.json(user);
-    } catch (error) {
-      res.status(500).json({ error: 'Server error' });
+  const { username, email, password } = req.body;
+
+  try {
+    const user = await User.findOne(email);
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
     }
-  });
 
-// Register a new user
-router.post('/register', async (req, res) => {
-    const { username, email, password } = req.body;
+    user.username = username || user.username;
+    user.email = email || user.email;
 
-
-    try {
-        // Check if the user already exists
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
-            return res.status(400).json({ error: 'Email already in use' });
-        }
-
-        // Hash the password
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        // Create a new user
-        const newUser = new User({
-            username,
-            email,
-            password: hashedPassword,
-        });
-
-        // Save the user to the database
-        await newUser.save();
-
-        res.status(201).json({ message: 'User registered successfully' });
-    } catch (error) {
-        res.status(500).json({ error: 'Server error' });
+    if (password) {
+      user.password = await bcrypt.hash(password, 10);
     }
+
+    await user.save();
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: 'Server error' });
+  }
 });
 
 
 
+//===========================Register a new user=========================================
+
+
+router.post('/register', async (req, res) => {
+
+  const { username, email, password } = req.body;
+
+
+
+
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ error: 'Email already in use' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+
+//=============================== login=======================================
 
 router.post('/login', async (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = JSON.parse(req.body)
 
-    try {
-
-        console.log("inside try login");
-
-        // Find the user by email
-        const user = await User.findOne({ email });
-        console.log(user);
-        if (!user) {
-            return res.status(400).json({ error: 'Invalid email or password' });
-        }
-
-        // Check if the password matches
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) {
-            return res.status(400).json({ error: 'Invalid email or password' });
-        }
-
-        // Send the token in the response
+  console.log(email, password, "email", "password");
 
 
-        res.status(200).json({ "User": "Valid" });
-    } catch (error) {
-        res.status(500).json({ error: 'Server error' });
+  try {
+
+    console.log("inside try login");
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ error: 'Invalid email or password' });
     }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ error: 'Invalid email or password' });
+    }
+
+    res.status(200).json({
+      _id: user._id,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+
+
+  } catch (error) {
+    res.status(500).json({ "error": 'Server error' });
+  }
 });
 
 module.exports = router;
