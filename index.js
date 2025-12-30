3cd65f52611aeaf2959cf250138c2055b8a304b4
Shivam Tiwari <shivamtiwari6223@gmail.com>
2025-12-30 15:46:14 +0530

    updated origin

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-    origin: ["https://kmcian.netlify.app", "https://kmcian.vercel.app"],
+    origin: ["https://kmcian.netlify.app", "https://kmcian.vercel.app", "kmcianbackend.vercel.app"],
     methods: 'GET,HEAD,PUT,POST,DELETE',
     credentials: true,
 }));
