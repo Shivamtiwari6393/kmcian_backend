2a13fa96dc55db087f3323dbee90a7fbaa3ba742
Shivam Tiwari <shivamtiwari6223@gmail.com>
2025-12-31 14:43:29 +0530

    protect reg endpoint

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { registerUser, loginUser, updateUser } = require("../controllers/userController")
+const { registerUser, loginUser, updateUser } = require("../controllers/userController");
+const protect = require('../middleware/auth');
 const router = express.Router();
 
 
-router.post('/register', registerUser);
+router.post('/register',protect, registerUser);
 router.put('/update', updateUser);
 router.post('/login', loginUser);
 
