5774744db272e3cd7173895e0aebae3393b309b9
Shivam Tiwari <shivamtiwari6223@gmail.com>
2025-12-31 13:38:16 +0530

    fixed short delete logic to delete metadata from db

--- controllers/ShortController.js
+++ controllers/ShortController.js
@@ -4,12 +4,9 @@ const Short = require("../models/ShortModel.js");
 const deleteShort = async (req, res) => {
     try {
         const { id } = req.params;
-        console.log(id);
-
-
+        // console.log(id);
         const short = await Short.findById(id);
-        console.log(short);
-
+        // console.log(short);
         if (!short) {
             return res.status(404).json({ message: "Short not found" });
         }
@@ -19,21 +16,11 @@ const deleteShort = async (req, res) => {
             const del = await cloudinary.uploader.destroy(short.publicId, {
                 resource_type: "video",
             });
-
-
-            console.log("-------------", del);
-
-
+            // console.log("-------------", del);
             if (del.result === "ok") return res.json({ message: "Short deleted successfully" });
             return res.status(404).json({ message: del.result })
-
         }
-
-        // await short.deleteOne();
-
-
-
-
+        await short.deleteOne();
     } catch (err) {
         console.error(err);
         res.status(500).json({ message: "Server error" });
